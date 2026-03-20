6562638ffb6a0668f24c8e82731a4626e303c98f
Agusbullrich <agusbullrich08@gmail.com>
2026-03-20 12:52:20 -0300

    Add files via upload

--- exercise_circle.py
+++ exercise_circle.py
@@ -7,6 +7,11 @@ def circle():
 
     Dado el radio de un círculo, calcular e imprimir:
     1. El área (π × radio²)
-    2. La circunferencia (2 × π × radio)
+    2. La circunferencia (2   × π × radio)
     """
     radio = 5
+    area = pi * radio ** 2
+    circunferencia = 2 * pi * radio
+    print(area)
+    print(circunferencia)
+circle()
--- exercise_rectangle.py
+++ exercise_rectangle.py
@@ -8,3 +8,9 @@ def rectangle():
     """
     base = 10
     altura = 5
+    area = base * altura
+    perimetro = (base + altura) * 2
+    print(area)
+    print(perimetro)
+
+rectangle()
